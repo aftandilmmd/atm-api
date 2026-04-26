97baccfad2352ba69948fbb05d1e914e30e7d5bf
aftandilmmd <aftandil.mmd@gmail.com>
2026-04-26 14:39:25 +0300

    v1 API marşrutlarını qeyd et və domain exceptionları üçün JSON render-lərini qoş

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Exceptions\CannotDispenseException;
+use App\Exceptions\InsufficientBalanceException;
+use App\Exceptions\TransactionAlreadyReversedException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,9 +14,19 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (InsufficientBalanceException $e, Request $req) {
+            return response()->json(['error' => 'INSUFFICIENT_BALANCE', 'message' => $e->getMessage()], 422);
+        });
+
+        $exceptions->render(function (CannotDispenseException $e, Request $req) {
+            return response()->json(['error' => 'CANNOT_DISPENSE', 'message' => $e->getMessage()], 422);
+        });
+
+        $exceptions->render(function (TransactionAlreadyReversedException $e, Request $req) {
+            return response()->json(['error' => 'TRANSACTION_ALREADY_REVERSED', 'message' => $e->getMessage()], 422);
+        });
     })->create();
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\WithdrawController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::get('accounts', [AccountController::class, 'index']);
+    Route::get('accounts/{account}', [AccountController::class, 'show']);
+    Route::get('accounts/{account}/transactions', [TransactionController::class, 'forAccount']);
+    Route::get('transactions/{transaction}', [TransactionController::class, 'show']);
+    Route::post('accounts/{account}/withdraw', WithdrawController::class);
+});
