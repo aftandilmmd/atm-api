dc35923b963264795c73c424a74c5bc5e96ac56e
aftandilmmd <aftandil.mmd@gmail.com>
2026-04-26 15:32:17 +0300

    withdraw route-una throttle:5,1 rate limit tətbiq et

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\WithdrawController;
 use App\Http\Middleware\RequireIdempotencyKey;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([RequireIdempotencyKey::class])->prefix('v1')->group(function () {
+Route::prefix('v1')->group(function () {
     Route::get('accounts', [AccountController::class, 'index']);
     Route::get('accounts/{account}', [AccountController::class, 'show']);
     Route::get('accounts/{account}/transactions', [TransactionController::class, 'forAccount']);
     Route::get('transactions/{transaction}', [TransactionController::class, 'show']);
-    Route::post('accounts/{account}/withdraw', WithdrawController::class);
+
+    Route::middleware(['throttle:5,1', RequireIdempotencyKey::class])->group(function () {
+        Route::post('accounts/{account}/withdraw', WithdrawController::class);
+    });
 });
