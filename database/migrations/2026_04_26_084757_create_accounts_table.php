c8dde11714473399a16a9a93875d539dc6ec4eef
aftandilmmd <aftandil.mmd@gmail.com>
2026-04-26 11:48:27 +0300

    account model və migration yarat

---
+++ database/migrations/2026_04_26_084757_create_accounts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('currency_id')->constrained();
+            $table->foreignId('user_id')->nullable()->constrained();
+            $table->string('owner_name');
+            $table->unsignedBigInteger('balance')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
