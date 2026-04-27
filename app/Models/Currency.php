6492ecd984c6105209dc870c32d843f66b6aa138
aftandilmmd <aftandil.mmd@gmail.com>
2026-04-27 09:12:50 +0300

    HasFactory import edildi

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['code', 'name'])]
 class Currency extends Model
 {
+    use HasFactory;
+
     public function denominations(): HasMany
     {
         return $this->hasMany(Denomination::class);
